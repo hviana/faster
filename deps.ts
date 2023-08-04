d7fd125736c6cc54cb33047d3aa79aefadf2c42a
Henrique <hv5088@gmail.com>
2023-08-04 11:37:27 -0300

    update deps

--- deps.ts
+++ deps.ts
@@ -1,30 +1,30 @@
-export { join, SEP } from "https://deno.land/std@0.184.0/path/mod.ts";
+export { join, SEP } from "https://deno.land/std@0.197.0/path/mod.ts";
 export {
   ensureDir,
   ensureDirSync,
   ensureFile,
   ensureFileSync,
   move,
-} from "https://deno.land/std@0.184.0/fs/mod.ts";
-export { crypto } from "https://deno.land/std@0.184.0/crypto/mod.ts";
+} from "https://deno.land/std@0.197.0/fs/mod.ts";
+export { crypto } from "https://deno.land/std@0.197.0/crypto/mod.ts";
 export {
   copy,
   readableStreamFromReader,
   readerFromStreamReader,
-} from "https://deno.land/std@0.184.0/streams/mod.ts";
+} from "https://deno.land/std@0.197.0/streams/mod.ts";
 export {
   generateSecret,
   jwtVerify,
   SignJWT,
-} from "https://deno.land/x/jose@v4.14.1/index.ts";
+} from "https://deno.land/x/jose@v4.14.4/index.ts";
 
 export { storage } from "https://deno.land/x/fast_storage/mod.ts";
 
-export type { Cookie } from "https://deno.land/std@0.184.0/http/cookie.ts";
+export type { Cookie } from "https://deno.land/std@0.197.0/http/cookie.ts";
 
 export {
   deleteCookie,
   getCookies,
   getSetCookies,
   setCookie,
-} from "https://deno.land/std@0.184.0/http/cookie.ts";
+} from "https://deno.land/std@0.197.0/http/cookie.ts";
