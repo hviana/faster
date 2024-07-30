e003354dc3a6654d2044bf68a5dfd5dee26b3ed5
Henrique Emanoel Viana <hv5088@gmail.com>
2024-07-30 13:02:15 -0300

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,27 +1,27 @@
-export { join } from "https://deno.land/std@0.222.1/path/mod.ts";
+export { join } from "https://deno.land/std@0.224.0/path/mod.ts";
 export {
   ensureDir,
   ensureDirSync,
   ensureFile,
   ensureFileSync,
   move,
-} from "https://deno.land/std@0.222.1/fs/mod.ts";
-export { crypto } from "https://deno.land/std@0.222.1/crypto/mod.ts";
+} from "https://deno.land/std@0.224.0/fs/mod.ts";
+export { crypto } from "https://deno.land/std@0.224.0/crypto/mod.ts";
 export {
   toReadableStream,
   toWritableStream,
-} from "https://deno.land/std@0.222.1/io/mod.ts";
+} from "https://deno.land/std@0.224.0/io/mod.ts";
 export {
   generateSecret,
   jwtVerify,
   SignJWT,
-} from "https://deno.land/x/jose@v5.2.4/index.ts";
+} from "https://deno.land/x/jose@v5.6.3/index.ts";
 
-export type { Cookie } from "https://deno.land/std@0.222.1/http/cookie.ts";
+export type { Cookie } from "https://deno.land/std@0.224.0/http/cookie.ts";
 
 export {
   deleteCookie,
   getCookies,
   getSetCookies,
   setCookie,
-} from "https://deno.land/std@0.222.1/http/cookie.ts";
+} from "https://deno.land/std@0.224.0/http/cookie.ts";
